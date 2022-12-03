a05871f89f6cd1dd5f503b43c7da44a1bf71a971
kati hajar <hakati1999@hotmail.com>
2022-12-03 19:20:52 +0100

    cc

--- src/main/java/com/example/demo/rest/PaymentRest.java
+++ src/main/java/com/example/demo/rest/PaymentRest.java
@@ -4,11 +4,9 @@ import com.example.demo.bean.Payment;
 import com.example.demo.repository.PaymentRepo;
 import com.example.demo.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentRest {
